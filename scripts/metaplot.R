ef5902ef833e30ddd41a05a3a957735ab6ceb40f
Your Name <etholleman@ucdavis.edu>
2021-05-16 22:22:06 -0700

    debug

--- scripts/metaplot.R
+++ scripts/metaplot.R
@@ -23,13 +23,23 @@ standard_error <- function(x){
   sd(x)/sqrt(length(x))
 }
 
+confidence_interval <- function(x){
+
+  n <- length(x)
+  s <- sd(x)
+  qnorm(0.975)*s/sqrt(n)
+
+}
+
+range01 <- function(x){(x-min(x))/(max(x)-min(x))}
+
 read_meta <- function(file.path){
   file.path <- as.character(file.path)
 
   print(file.path)
   print(typeof(file.path))
   df <- as.data.frame(read.table(file.path, header=F))
-  df.agg <- as.data.frame(aggregate(df, list(df$V1), function(x) c(mean=mean(x), se=standard_error(x))))
+  df.agg <- as.data.frame(aggregate(df, list(df$V1), function(x) c(mean=mean(x), se=confidence_interval(x))))
   # aggregate by bin, taking the mean and standard error for each bin
   # return just bin, mean and se
   region <- get_region_from_input_file(file.path)
@@ -41,21 +51,21 @@ read_meta <- function(file.path){
   df.agg  
 }
 
-plot_meta_region <- function(df.agg, region.name){
+plot_meta_region <- function(df.agg){
 
     # by_region <- ggplot(df.agg, aes(x=as.numeric(bin), y=mean, color=sample)) + theme_pubclean() +
     # labs(x='bins') + 
     # geom_ribbon(aes(ymax=mean+se, ymin=mean-se, x=as.numeric(bin), fill=sample), alpha=0.7) +
     # geom_line(color='black') + facet_wrap(~region)
-    df.agg.region <- subset(df.agg, region==region.name)
-    n_samples <- unique(df.agg.region$sample)
+    #df.agg.region <- subset(df.agg, region==region.name)
+    n_samples <- unique(df.agg$sample)
     colors <- colorRampPalette(brewer.pal(8, "Dark2"))(length(n_samples))
 
-    by_sample <- ggplot(df.agg.region, aes(x=as.numeric(bin), y=mean)) + theme_pubr() +
+    by_sample <- ggplot(df.agg, aes(x=as.numeric(bin), y=mean)) + theme_pubr() +
     labs(x='bins') + 
     geom_ribbon(aes(ymax=mean+se, ymin=mean-se, x=as.numeric(bin), fill=sample), alpha=0.4) +
-    geom_line(aes(color=sample)) + facet_wrap(~sample) + scale_color_manual(values=colors) + 
-    scale_fill_manual(values=colors) + theme(legend.position = "none") + labs(main=region.name)
+    geom_line(aes(color=sample)) + facet_grid(vars(sample), vars(region), scales="free") + scale_color_manual(values=colors) + 
+    scale_fill_manual(values=colors) + theme(legend.position = "none")
 
     by_sample
 
@@ -89,6 +99,7 @@ plot_meta_region <- function(df.agg, region.name){
 
 # }
 
+
 input.files <- snakemake@input
 dfs <- list()
 for (i in 1:length(input.files)){
@@ -102,19 +113,19 @@ region_plts <- list()
 regions <- unique(big.df$region)
 
 
-for (i in 1:length(regions)){
+# for (i in 1:length(regions)){
 
-  region_plts[[i]] <- plot_meta_region(big.df, regions[[i]])
+#   region_plts[[i]] <- plot_meta_region(big.df, regions[[i]])
 
-}
+# }
 
 save.image()
 
 alphabet <- c('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H')
 
+plt <- plot_meta_region(big.df)
 
-
-plt <- ggarrange(plotlist=region_plts, labels=regions, nrow=length(region_plts), ncol=1)
+#plt <- ggarrange(plotlist=region_plts, labels=regions, nrow=length(region_plts), ncol=1)
 ggsave(as.character(snakemake@output), plt, width=14, height=10, unit='in')
 
 
