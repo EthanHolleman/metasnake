7193b776d7faf80945b5ea69e18fa07b313ed3fc
Your Name <etholleman@ucdavis.edu>
2021-05-17 20:23:13 -0700

    update

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 mkdir -p logs
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate snakemake
-snakemake -j 14 -s snakefile --cluster-config cluster.yml \
+snakemake -j 24 -s snakefile --cluster-config cluster.yml \
 --cluster "sbatch -p {cluster.partition} -t {cluster.time} -N {cluster.nodes} \
 -n {cluster.cpus} --mem {cluster.mem} -J {cluster.name} -o {cluster.output} \
 -e {cluster.output} --mail-type ALL --mail-user {cluster.email}" \
